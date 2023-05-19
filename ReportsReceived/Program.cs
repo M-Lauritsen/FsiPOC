ec22fa5c819fd3590f28abe0a5ee136fb6004440
Michael Gottschalk-Lauritsen <MIGL@nnit.com>
2023-05-19 15:12:16 +0200

    Add project files.

---
+++ ReportsReceived/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "FSI", type: ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "*.*";
+var message = (args.Length > 1)
+              ? string.Join(" ", args.Skip(1).ToArray())
+              : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "FSI",
+                     routingKey: routingKey,
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
